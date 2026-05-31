f8fac5e0d91b9b4b3080195941b5fd06313286a0
kaneezfatima123-af <kaneezfatima15078@email.com>
2026-05-31 05:35:57 -0700

    added vercel.json

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://fatima-elearning.netlify.app'
+}));
 app.use(express.json());
 app.use(express.static(__dirname));
 
@@ -29,10 +31,6 @@ app.use('/api/comments', commentRoutes);
 
 // MongoDB Connect
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('✅ MongoDB Connected!');
-    app.listen(process.env.PORT || 3000, () => {
-      console.log(`🚀 Server running on http://localhost:${process.env.PORT || 3000}`);
-    });
-  })
   .catch(err => console.error('❌ MongoDB Error:', err));
+
+export default app;
